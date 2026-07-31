531ea0e371f48677280e2e66081a2b47e296547c
latrelheron <latrel.heron@outlook.com>
2026-07-31 19:08:13 +1000

    Removed display demo as they were successful in displaying content and having a changing sequnce emulating time functions, in preperation for temp sensor tests

--- rp2040/src/board.h
+++ rp2040/src/board.h
@@ -1,6 +1,17 @@
 #pragma once
 
+#include "hardware/i2c.h"
+#include "pico/types.h"
 
+namespace board
+{
+    inline i2c_inst_t *const I2C_PORT = i2c0;
+
+    constexpr uint I2C_SDA_PIN = 16;
+    constexpr uint I2C_SCL_PIN = 17;
+
+    constexpr uint I2C_BAUD_RATE = 100000;
+}
 
 
 #define LED_PIN 14
