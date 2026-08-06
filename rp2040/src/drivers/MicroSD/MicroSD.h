27fe7e9b6ac4f0d01778e2a205b20c86ce276afd
Harry McRae <jc972155@my.jcu.edu.au>
2026-08-06 12:55:28 +1000

    add fatfs files to drivers folder

---
+++ rp2040/src/drivers/MicroSD/MicroSD.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <string>
+#include "hardware/spi.h"
+
+extern "C"
+{
+#include "ff.h"
+}
+
+class MicroSD {
+    public:
+        MicroSD(
+            spi_inst_t* spi,
+            uint dat3,
+            uint cmd,
+            uint clk,
+            uint dat0,
+            uint cd,
+            uint baudrate
+        );
+
+        bool init();
+
+        bool mount();
+
+        bool isMounted() const;
+
+        bool WriteFile(const std::string& filename,
+                        const std::string& text);
+
+        bool appendFile(const std::string& filename,
+                        const std::string& text);
+
+        bool readFile(const std::string& filename,
+                  std::string& output);
+
+        bool exists(const std::string& filename);
+
+        bool remove(const std::string& filename);
+    
+    private:
+        spi_inst_t* _spi;
+
+        uint _mosi;
+        uint _miso;
+        uint _sck;
+        uint _cs;
+
+        uint _baudrate;
+
+        FATFS _fatfs;
+
+        bool _mounted;
+};
