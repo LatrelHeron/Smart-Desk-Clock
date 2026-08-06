be3561f19a2de34b24dd4db48425af2d761276cd
latrelheron <latrel.heron@outlook.com>
2026-08-06 19:43:42 +1000

    Format board header file

--- rp2040/src/board.h
+++ rp2040/src/board.h
@@ -17,24 +17,23 @@ namespace board
     // SPI bus for the e-paper display.
     inline spi_inst_t *const EPD_SPI = spi0;
 
-    constexpr uint EPD_MOSI_PIN  = 19;
-    constexpr uint EPD_SCK_PIN   = 22;
-    constexpr uint EPD_CS_PIN    = 21;
-    constexpr uint EPD_DC_PIN    = 23;
-    constexpr uint EPD_RST_PIN   = 24;
-    constexpr uint EPD_BUSY_PIN  = 25;
+    constexpr uint EPD_MOSI_PIN = 19;
+    constexpr uint EPD_SCK_PIN = 22;
+    constexpr uint EPD_CS_PIN = 21;
+    constexpr uint EPD_DC_PIN = 23;
+    constexpr uint EPD_RST_PIN = 24;
+    constexpr uint EPD_BUSY_PIN = 25;
 
     // Bus for SD card.
     constexpr uint SD_DAT3_PIN = 5;
-    constexpr uint SD_CLK_PIN  = 6;
-    constexpr uint SD_CMD_PIN  = 7;
-    constexpr uint SD_DAT0_PIN   = 8;
-    constexpr uint SD_CD_PIN   = 9;
+    constexpr uint SD_CLK_PIN = 6;
+    constexpr uint SD_CMD_PIN = 7;
+    constexpr uint SD_DAT0_PIN = 8;
+    constexpr uint SD_CD_PIN = 9;
 
     constexpr uint SD_BAUD_RATE = 1000000;
 
 }
-
 
 #define LED_PIN 14
 #define LED_COUNT 12
