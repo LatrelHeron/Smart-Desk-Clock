ce4f5684b201d65867ee25f5cd6f39132c13c482
jc972155 <HARRY.MCRAE@MY.JCU.EDU.AU>
2026-08-13 16:10:05 +1000

    improve date change in time_adjust

--- rp2040/src/time_adjust.cpp
+++ rp2040/src/time_adjust.cpp
@@ -1,15 +1,14 @@
 #include "time_adjust.h"
 #include <stdio.h>
 
-TimeAdjust::TimeAdjust(INS5699S& rtc, volatile bool& btn1_flag, volatile bool& btn2_flag)
-    : _rtc(rtc), _btn1(btn1_flag), _btn2(btn2_flag) {}
+TimeAdjust::TimeAdjust(INS5699S& rtc, volatile bool& btn1_flag, volatile bool& btn2_flag, volatile bool& btn3_flag)
+    : _rtc(rtc), _btn1(btn1_flag), _btn2(btn2_flag), _btn3(btn3_flag) {}
 
 void TimeAdjust::advance_state() {
     switch (_state) {
-        case State::EDIT_YEAR: _state = State::EDIT_MONTH; printf("Editing: MONTH\n"); break;
-        case State::EDIT_MONTH: _state = State::EDIT_DATE; printf("Editing: DATE\n"); break;
-        case State::EDIT_DATE: _state = State::EDIT_DAY; printf("Editing: WEEKDAY\n"); break;
-        case State::EDIT_DAY: _state = State::EDIT_HOUR; printf("Editing: HOUR\n"); break;
+        case State::EDIT_DAY: _state = State::EDIT_MONTH; printf("Editing: MONTH\n"); break;
+        case State::EDIT_MONTH: _state = State::EDIT_YEAR; printf("Editing: DATE\n"); break;
+        case State::EDIT_YEAR: _state = State::EDIT_HOUR; printf("Editing: HOUR\n"); break;
         case State::EDIT_HOUR:   _state = State::EDIT_MINUTE; printf("Editing: MINUTE\n"); break;
         case State::EDIT_MINUTE: _state = State::EDIT_SECOND; printf("Editing: SECOND\n"); break;
         case State::EDIT_SECOND: _state = State::DONE; break;
@@ -19,9 +18,9 @@ void TimeAdjust::advance_state() {
 
 void TimeAdjust::increment_field() {
     switch (_state) {
-        case State::EDIT_YEAR:   _buffer.year   = (_buffer.year + 1) % 2099; printf("Hour: %02u\n", _buffer.year); break;
-        case State::EDIT_MONTH:   _buffer.month   = (_buffer.month + 1) % 12; printf("Hour: %02u\n", _buffer.month); break;
-        case State::EDIT_DAY:   _buffer.day   = (_buffer.day + 1) % 31; printf("Hour: %02u\n", _buffer.day); break;
+        case State::EDIT_DAY:   _buffer.day   = (_buffer.day + 1) % 31; printf("Day: %02u\n", _buffer.day); break;
+        case State::EDIT_MONTH:   _buffer.month   = ((_buffer.month + 1) % 12) + 1; printf("Month: %02u\n", _buffer.month); break;
+        case State::EDIT_YEAR:   _buffer.year   = ((_buffer.year + 1) % 99) + 2000; printf("Year: %04u\n", _buffer.year); break;
         case State::EDIT_HOUR:   _buffer.hour   = (_buffer.hour + 1) % 24; printf("Hour: %02u\n", _buffer.hour); break;
         case State::EDIT_MINUTE: _buffer.minute = (_buffer.minute + 1) % 60; printf("Minute: %02u\n", _buffer.minute); break;
         case State::EDIT_SECOND: _buffer.second = (_buffer.second + 1) % 60; printf("Second: %02u\n", _buffer.second); break;
@@ -31,8 +30,8 @@ void TimeAdjust::increment_field() {
 
 void TimeAdjust::run() {
     _buffer = _rtc.read_datetime(); // snapshot to edit
-    _state = State::EDIT_YEAR;
-    printf("Entering time edit: YEAR\n");
+    _state = State::EDIT_DAY;
+    printf("Entering time edit: DAY\n");
 
     while (_state != State::DONE) {
         if (_btn1) { _btn1 = false; advance_state(); }
--- rp2040/src/time_adjust.h
+++ rp2040/src/time_adjust.h
@@ -4,17 +4,18 @@
 class TimeAdjust {
 public:
     // References to the ISR-set flags declared in main.cpp
-    TimeAdjust(INS5699S& rtc, volatile bool& btn1_flag, volatile bool& btn2_flag);
+    TimeAdjust(INS5699S& rtc, volatile bool& btn1_flag, volatile bool& btn2_flag, volatile bool& btn3_flag);
 
     // Blocks until the user has stepped through hour/minute/second and saved.
     void run();
 
 private:
-    enum class State { EDIT_YEAR, EDIT_MONTH, EDIT_DATE, EDIT_DAY, EDIT_HOUR, EDIT_MINUTE, EDIT_SECOND, DONE };
+    enum class State { EDIT_DAY, EDIT_MONTH, EDIT_YEAR, EDIT_HOUR, EDIT_MINUTE, EDIT_SECOND, DONE };
 
     INS5699S& _rtc;
     volatile bool& _btn1;
     volatile bool& _btn2;
+    volatile bool& _btn3;
     DateTime _buffer{};
     State _state = State::EDIT_YEAR;
 
